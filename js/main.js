b6add623fdaa7e4a5a850f45d0e8bff74a75c8d3
Bruna Costa <bantonellodj@gmail.com>
2022-07-16 15:51:30 -0300

    Atualizando parte servicos

---
+++ js/main.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function ($) {
+ //fixando header
+ window.onscroll = function(){
+  if (window.pageXOffset > 140){
+   $("#header").addClass("active");
+ } else {
+   $("#header").removeClass("active");
+ }
+ };
+
+ //isotopo = ao clicar em qualquer dos botões 
+ //ele vai remover a classe ativa do botão e transferir para os outros.
+ let btns = $("#servicos .button-group button");
+
+ btns.click(function(e) {
+   $("servicos .button-group button").removeClass("active");
+   e.target.classList.add("active");
+
+ let selector = $(e.target).attr("data-filter");
+ $("#servicos .grid").isotope({
+  filter: selector,
+ });
+ });
+
+$(window).on("load", function() {
+  $("#servicos .grid").isotope({
+    filter: "*",
+   });
+
+});
+
+//magnify
+
+$(".grid .popup-link").magnificPopup({
+  type: "image", 
+  gallery: {
+    enabled: true,
+    tPrev:"Anterior",
+    tNext: "Póxima",
+    tCounter: "%curr% de %total%",
+  },
+})
+
+})
